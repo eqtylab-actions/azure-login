bd8a20eb47826980c0e8dcd39c6248aaaf2e6646
shiyingchen <shiyingchen@microsoft.com>
2023-06-07 15:40:34 +0800

    support managed identity

--- src/common/LoginConfig.ts
+++ src/common/LoginConfig.ts
@@ -15,15 +15,10 @@ export class LoginConfig {
     subscriptionId: string;
     resourceManagerEndpointUrl: string;
     allowNoSubscriptionsLogin: boolean;
-    enableOIDC: boolean;
     environment: string;
     enableAzPSSession: boolean;
     audience: string;
     federatedToken: string;
-
-    constructor() {
-        this.enableOIDC = true;
-    }
 
     async initialize() {
         this.environment = core.getInput("environment").toLowerCase();
@@ -35,25 +30,24 @@ export class LoginConfig {
         this.tenantId = core.getInput('tenant-id', { required: false });
         this.subscriptionId = core.getInput('subscription-id', { required: false });
 
-        this.audience = core.getInput('audience', { required: false });
-        this.federatedToken = null;
         let creds = core.getInput('creds', { required: false });
         let secrets = creds ? new SecretParser(creds, FormatType.JSON) : null;
-
         if (creds) {
-            core.debug('using creds JSON...');
-            this.enableOIDC = false;
-            this.servicePrincipalId = secrets.getSecret("$.clientId", true);
-            this.servicePrincipalKey = secrets.getSecret("$.clientSecret", true);
-            this.tenantId = secrets.getSecret("$.tenantId", true);
-            this.subscriptionId = secrets.getSecret("$.subscriptionId", true);
+            core.debug('Reading creds in JSON...');
+            this.servicePrincipalId = this.servicePrincipalId ? this.servicePrincipalId : secrets.getSecret("$.clientId", false);
+            this.servicePrincipalKey = secrets.getSecret("$.clientSecret", false);
+            this.tenantId = this.tenantId ? this.tenantId : secrets.getSecret("$.tenantId", false);
+            this.subscriptionId = this.subscriptionId ? this.subscriptionId : secrets.getSecret("$.subscriptionId", false);
             this.resourceManagerEndpointUrl = secrets.getSecret("$.resourceManagerEndpointUrl", false);
         }
+
+        this.audience = core.getInput('audience', { required: false });
+        this.federatedToken = null;
         this.getFederatedTokenIfNecessary();
     }
 
     async getFederatedTokenIfNecessary() {
-        if (!this.enableOIDC) {
+        if (this.servicePrincipalKey) {
             return;
         }
         try {
@@ -63,24 +57,13 @@ export class LoginConfig {
             core.error(`Please make sure to give write permissions to id-token in the workflow.`);
             throw error;
         }
-        if (!!this.federatedToken) {
-            let [issuer, subjectClaim] = await jwtParser(this.federatedToken);
-            console.log("Federated token details: \n issuer - " + issuer + " \n subject claim - " + subjectClaim);
-        }
-        else {
-            throw new Error("Failed to fetch federated token.");
-        }
+        let [issuer, subjectClaim] = await jwtParser(this.federatedToken);
+        console.log("Federated token details:\n issuer - " + issuer + "\n subject claim - " + subjectClaim);
     }
 
     async validate() {
-        if (!this.servicePrincipalId || !this.tenantId || !(this.servicePrincipalKey || this.enableOIDC)) {
-            throw new Error("Not all values are present in the credentials. Ensure clientId, clientSecret and tenantId are supplied.");
-        }
-        if (!this.subscriptionId && !this.allowNoSubscriptionsLogin) {
-            throw new Error("Not all values are present in the credentials. Ensure subscriptionId is supplied.");
-        }
         if (!LoginConfig.azureSupportedCloudName.has(this.environment)) {
-            throw new Error("Unsupported value for environment is passed.The list of supported values for environment are ‘azureusgovernment', ‘azurechinacloud’, ‘azuregermancloud’, ‘azurecloud’ or ’azurestack’");
+            throw new Error("Unsupported value for environment is passed. The list of supported values for environment are 'azureusgovernment', 'azurechinacloud', 'azuregermancloud', 'azurecloud' or 'azurestack'");
         }
     }
 }
